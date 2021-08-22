60413e31d7109ce45d4c01cf0137ee23f893907b
tanupoo <github-shoichi@tanu.org>
2021-08-23 01:39:30 +0900

    fixed to handle multiple targets.

--- build.sh
+++ build.sh
@@ -11,23 +11,27 @@ usage()
 
 build_image()
 {
-    name=$1
-    echo "==== Build ${name} ===="
-    cd ${name} && \
-    if [ "`docker images ${name}:build -q`" ] ; then
-        docker rmi ${name}:build ;
-    fi
-    docker build -t ${name}:build --build-arg __NC=`date +%Y%m%d%H%M%S` .
+    (
+        name=$1
+        echo "==== Build ${name} ===="
+        cd ${name} && \
+        if [ "`docker images ${name}:build -q`" ] ; then
+            docker rmi ${name}:build ;
+        fi
+        docker build -t ${name}:build --build-arg __NC=`date +%Y%m%d%H%M%S` .
+    )
 }
 
 push_image()
 {
-    name=$1
-    tag=`grep ${name} latest-versions.txt | cut -d: -f2`
-    pubname=tanupoo/${name}:${tag}
-    echo "==== Push ${pubname} ===="
-    docker tag ${name}:build ${pubname} && \
-    docker push ${pubname}
+    (
+        name=$1
+        tag=`grep ${name} latest-versions.txt | cut -d: -f2`
+        pubname=tanupoo/${name}:${tag}
+        echo "==== Push ${pubname} ===="
+        docker tag ${name}:build ${pubname} && \
+        docker push ${pubname}
+    )
 }
 
 #
